3903447a1ec6f84e18a8e9bc880256a74a10839d
Ali Fahmi <fahmi.aa@gmail.com>
2021-02-04 17:52:02 +0700

    modified file path to ask user input

--- doitall.sh
+++ doitall.sh
@@ -1,10 +1,14 @@
+[ $# -lt 1 ] && echo "Usage: $0 filepath" && exit 1
+
+dir=$1
+
 for sta in MEA{01..05};
 do 
 	for yr in {2013..2020}; 
 	do 
 		for mo in {01..12}; 
 		do 
-			./script.sh /DOMERAPI/data3/ANTENNA/GCF $sta $yr $mo
+			./script.sh $dir $sta $yr $mo
 		done
 
 		./combine $sta $yr
